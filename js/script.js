77c45cb24e4764e677bf0a9c3e3ce41db506e11f
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-09-13 16:45:36 +0200

    Add animation on the svg on the hero section, fixed the sticky position on the aside section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const header = document.querySelector("header");
 const hero = document.querySelector(".hero");
+const mSvg = document.querySelectorAll(".m-svg svg> g>g>g");
 
 window.addEventListener("scroll", function () {
   // Ottieni la posizione del top dell'elemento
@@ -13,3 +14,16 @@ window.addEventListener("scroll", function () {
     header.classList.remove("changeColor");
   }
 });
+
+const toggleOpacity = () => {
+  const currentG = mSvg[Math.floor(Math.random() * mSvg.length)];
+  const currentOpacity = parseFloat(currentG.getAttribute("opacity"));
+  currentG.setAttribute("opacity", currentOpacity === 0 ? 1 : 0);
+};
+
+const startRandomOpacityToggle = () => {
+  setInterval(toggleOpacity, Math.floor(Math.random() * 1000));
+};
+
+startRandomOpacityToggle();
+startRandomOpacityToggle();
